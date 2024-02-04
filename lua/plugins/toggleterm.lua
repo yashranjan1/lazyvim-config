d45dc9745f7e64c7da412bd9adaa458ae9c6ef7e
Yash <yash.ranjan25@gmail.com>
2024-02-04 15:45:34 +0530

    spacing changes and added powershell as main shell

--- lua/plugins/toggleterm.lua
+++ lua/plugins/toggleterm.lua
@@ -3,7 +3,9 @@ return {
         "akinsho/toggleterm.nvim",
         config = function()
             require("toggleterm").setup({
-                open_mapping = [[<c-\>]]
+                open_mapping = [[<c-\>]],
+                shell = "powershell",
+                size = 20,
             })
         end,
         keys = {
@@ -11,11 +13,6 @@ return {
             { "<leader>1", "<Cmd>1ToggleTerm<Cr>", desc = "Terminal #1" },
             { "<leader>2", "<Cmd>2ToggleTerm<Cr>", desc = "Terminal #2" },
             { "<leader>3", "<Cmd>3ToggleTerm<Cr>", desc = "Terminal #3" },
-            -- {
-            --     "<leader>bt",
-            --     "<cmd>ToggleTerm size=15 direction=horizontal<cr> ",
-            --     desc = "Open a horizontal terminal at the Desktop directory",
-            -- },
         },
     },
 }
